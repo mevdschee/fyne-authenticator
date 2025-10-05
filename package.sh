cbfc3af31ddfc470495afc674dcf4db88f6461de
Maurits van der Schee <maurits@vdschee.nl>
2025-10-05 18:35:39 +0200

    fix opengl requirement

--- package.sh
+++ package.sh
@@ -2,10 +2,10 @@
 #
 #go install github.com/fyne-io/fyne-cross@latest
 #
-~/go/bin/fyne-cross windows -arch=amd64
-~/go/bin/fyne-cross windows -arch=arm64
-~/go/bin/fyne-cross linux -arch=amd64
-~/go/bin/fyne-cross linux -arch=arm64
+~/go/bin/fyne-cross windows -arch=amd64 -tags=no_animations,gles
+~/go/bin/fyne-cross windows -arch=arm64 -tags=no_animations,gles
+~/go/bin/fyne-cross linux -arch=amd64 -tags=no_animations,gles
+~/go/bin/fyne-cross linux -arch=arm64 -tags=no_animations,gles
 mv fyne-cross/dist/linux-arm64/fyne-authenticator.tar.xz fyne-cross/dist/fyne-authenticator-arm64.tar.xz
 mv fyne-cross/dist/linux-amd64/fyne-authenticator.tar.xz fyne-cross/dist/fyne-authenticator-amd64.tar.xz
 mv fyne-cross/dist/windows-arm64/fyne-authenticator.exe.zip fyne-cross/dist/fyne-authenticator-arm64.exe.zip
